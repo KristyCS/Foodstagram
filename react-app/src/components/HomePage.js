6bb037f93931c3bffdb9f777cef5c907b9fa8214
Kristy Zhang <zhangjy1988@gmail.com>
2021-11-06 17:39:25 -0400

    add component of post card and home page

---
+++ react-app/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>My Home Page</h1>
+    </>
+  );
+};
+
+export default HomePage;
