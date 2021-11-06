4bbc74dfb54cd76246238d83f6845d6af2b879c5
aftonslone <afton@gmail.com>
2021-11-06 05:10:41 -0400

    Update

--- app/models/user.py
+++ app/models/user.py
@@ -50,7 +50,6 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password, password)
 
     def to_dict(self):
-        print(self.posts[0].__dict__)
         return {
             'id': self.id,
             'username': self.username,
