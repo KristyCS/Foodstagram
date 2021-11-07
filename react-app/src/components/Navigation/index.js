eed033d75a22dc51470f23832a24f773bc7ffd64
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-11-06 19:10:44 -0700

    nav bar displays appropriate links to logged in and logged out users

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,33 +1,43 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import LogoutButton from '../auth/LogoutButton';
 import './NavBar.css';
 
 const NavBar = () => {
+    const user = useSelector(state => state.session.user);
+    let sessionLinks;
 
-   let sessionLinks = (
-        <>
-            <div id="login-nav">
-                <NavLink id="nav-lbtn" to='/login' exact={true} activeClassName='active'>
-                    Log In
-                </NavLink>
-            </div>
-            <div id="signup-nav">
-                <NavLink id="nav-sbtn" to='/sign-up' exact={true} activeClassName='active'>
-                    Sign Up
-                </NavLink>
-            </div>
-            <div>
-                <NavLink to='/users' exact={true} activeClassName='active'>
-                    Users
-                </NavLink>
-            </div>
-            <div>
-                <LogoutButton />
-            </div>
-        </>
-    )
+    if (user) {
+        sessionLinks = (
+            <>
+                <div>
+                    <NavLink to='/users' exact={true} activeClassName='active'>
+                        Users
+                    </NavLink>
+                </div>
+                <div>
+                    <LogoutButton />
+                </div>
+            </>
+        )
+    } else {
+        sessionLinks = (
+            <>
+                <div id="login-nav">
+                    <NavLink id="nav-lbtn" to='/login' exact={true} activeClassName='active'>
+                        Log In
+                    </NavLink>
+                </div>
+                <div id="signup-nav">
+                    <NavLink id="nav-sbtn" to='/sign-up' exact={true} activeClassName='active'>
+                        Sign Up
+                    </NavLink>
+                </div>
+            </>
+        )
+    }
 
 
   return (
@@ -40,7 +50,6 @@ const NavBar = () => {
         <div id="nav-buttons">
             {sessionLinks}
         </div>
-
     </nav>
   );
 }
