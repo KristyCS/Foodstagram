1167f0d8f7f96bd396ad9c79bee1acd66edcfdce
Kristy Zhang <zhangjy1988@gmail.com>
2021-11-08 18:58:38 -0500

    create new post and save to aws

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -2,17 +2,25 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import SinglePostCard from "../SinglePostCard/SinglePostCard";
 import { getPosts } from "../../store/posts";
+import {Modal} from "../../contex/Modal"
+import CreatePostPage from "../CreatePostPage/CreatePostPage";
 const HomePage = () => {
   const despatch = useDispatch();
   const postsObjs = useSelector(state=> state.posts.allPosts);
   const postLists = Object.values(postsObjs)
+  const [showCreatePostModal, setShowCreatePostModal] = useState(false);
   useEffect(() => {
     despatch(getPosts());
   },[]);
 
   return (
     <>
-      <h1>My Home Page</h1>
+      <button onClick={()=>setShowCreatePostModal(true)}>Create a new Post</button>
+      {showCreatePostModal && (
+        <Modal onClose={() => setShowCreatePostModal(false)}>
+          <CreatePostPage setShowCreatePostModal={setShowCreatePostModal}/>
+        </Modal>
+      )}
       {postLists && (
         <div className="posts_containers">
           {postLists.map(post => (
