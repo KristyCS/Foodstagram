a9825548f641b5c53ed69d738e2f64e4c4d25e98
Kristy Zhang <zhangjy1988@gmail.com>
2021-11-09 10:19:20 -0500

    view a specified post

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import SinglePostCard from "../SinglePostCard/SinglePostCard";
-import { getPosts } from "../../store/posts";
-import {Modal} from "../../contex/Modal"
+import { getPosts, deletePost} from "../../store/posts";
+import { Modal } from "../../contex/Modal";
+import {AiOutlinePlusSquare} from "react-icons/ai";
 import CreatePostPage from "../CreatePostPage/CreatePostPage";
 const HomePage = () => {
   const despatch = useDispatch();
-  const postsObjs = useSelector(state=> state.posts.allPosts);
-  const postLists = Object.values(postsObjs)
+  const postsObjs = useSelector((state) => state.posts.allPosts);
+  const postLists = Object.values(postsObjs);
   const [showCreatePostModal, setShowCreatePostModal] = useState(false);
   useEffect(() => {
     despatch(getPosts());
-  },[]);
-
+  }, []);
+  const deletePostHandler = (id = 5) => {
+    despatch(deletePost(id));
+  };
+  
   return (
     <>
-      <button onClick={()=>setShowCreatePostModal(true)}>Create a new Post</button>
+      <AiOutlinePlusSquare onClick={() => setShowCreatePostModal(true)}>
+      </AiOutlinePlusSquare>
+      <button onClick={deletePostHandler}>Delte</button>
       {showCreatePostModal && (
-        <Modal onClose={() => setShowCreatePostModal(false)}>
-          <CreatePostPage setShowCreatePostModal={setShowCreatePostModal}/>
+        <Modal  onClose={() => setShowCreatePostModal(false)}>
+          <CreatePostPage setShowCreatePostModal={setShowCreatePostModal} />
         </Modal>
       )}
       {postLists && (
         <div className="posts_containers">
-          {postLists.map(post => (
+          {postLists.map((post) => (
             <SinglePostCard key={post.id} singlePost={post} />
           ))}
         </div>
