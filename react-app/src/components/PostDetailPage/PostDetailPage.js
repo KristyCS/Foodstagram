ccd82243f5a5c43223b6883a4a2769d108b7db93
Kristy Zhang <zhangjy1988@gmail.com>
2021-11-12 19:19:04 -0500

    css

--- react-app/src/components/PostDetailPage/PostDetailPage.js
+++ react-app/src/components/PostDetailPage/PostDetailPage.js
@@ -274,10 +274,7 @@ function PostDetailPage({
             src={singlePost.user.profile_photo}
             alt=""
           />
-          {/* <NavLink to={`/users/dashboard/${singlePost.user.username}`}>
-            {singlePost.user.username}
-          </NavLink> */}
-          <p className=""><NavLink to={`/users/dashboard/${singlePost.user.username}`}>
+          <p><NavLink to={`/users/dashboard/${singlePost.user.username}`}>
             {singlePost.user.username}
           </NavLink> {singlePost.description}</p>
         </div>
